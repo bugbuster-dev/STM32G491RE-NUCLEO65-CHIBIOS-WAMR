4ec264051c3bc55a8fa74fe91bac0430f09a63e4
bugbuster-dev <71820608+bugbuster-dev@users.noreply.github.com>
2024-03-25 13:05:12 +0000

    build.sh to build in clean codespace

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install gcc-arm-none-eabi
+sudo apt -y install gcc-arm-none-eabi
 
 git clone https://github.com/bugbuster-dev/ChibiOS.git
 git clone https://github.com/bugbuster-dev/wasm-micro-runtime.git wamr
